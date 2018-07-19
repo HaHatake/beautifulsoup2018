4b430c70379bc7b1ce097ac5cd3da74b7e3de9d8
HaHatake <hideahat@gmail.com>
2018-07-19 15:06:48 +0900

    Create taistingsoupv2.py

---
+++ taistingsoupv2.py
@@ -0,0 +1,11 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+def url_to_soup(url):
+    req = requests.get(url)
+    return BeautifulSoup(req.content, 'html.parser')
+    
+def horse_page_link(url):
+    soup = url_to_soup(url)
+    link_list = [HOME_URL + x.get('href') for x in soup.find_all('a', class_='tx-mid tx-low') ]
+    return link_list
